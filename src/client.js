e2366b3f0a70afdc6e40a9177dd8cdf94c60bbd8
clearloop <udtrokia@gmail.com>
2019-06-16 17:56:02 +0800

    babel-plugin-transform-commonjs

---
+++ src/client.js
@@ -0,0 +1,9 @@
+const r = require('./req');
+const store = require('store');
+
+class Client {
+  static verify() {
+    let author = store.get('author');
+    return r.get(`/_/verify?author=${author}`);
+  }
+}
